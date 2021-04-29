ae135c9e0a7f36e3dd4da5e899aaf4bb1b643cde
Kristen Picard <kristenpicard93@gmail.com>
2021-04-29 19:18:02 -0400

    Working on API routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,124 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Review, Comment, User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, (req, res) => {
+  Review.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "review_body", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: [
+          "id",
+          "comment_body",
+          "review_id",
+          "user_id",
+          "created_at",
+        ],
+        include: {
+          model: User,
+          attributes: ["name"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["name"],
+      },
+    ],
+  })
+    .then((reviewData) => {
+      const posts = reviewData.map((review) => review.get({ plain: true }));
+      res.render("dashboard", { reviews, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "created_at", "review_body"],
+    include: [
+      {
+        model: Comment,
+        attributes: [
+          "id",
+          "comment_body",
+          "review_id",
+          "user_id",
+          "created_at",
+        ],
+        include: {
+          model: User,
+          attributes: ["name"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["name"],
+      },
+    ],
+  })
+    .then((reviewData) => {
+      if (!reviewData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      const post = reviewData.get({ plain: true });
+      res.render("edit-post", {
+        post,
+        loggedIn: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/create/", withAuth, (req, res) => {
+  Review.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "created_at", "review_body"],
+    include: [
+      {
+        model: Comment,
+        attributes: [
+          "id",
+          "comment_body",
+          "review_id",
+          "user_id",
+          "created_at",
+        ],
+        include: {
+          model: User,
+          attributes: ["name"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["name"],
+      },
+    ],
+  })
+    .then((reviewData) => {
+      const reviews = reviewData.map((review) => review.get({ plain: true }));
+      res.render("create-post", { reviews, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,88 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Review, Comment, User } = require("../models");
+
+// GET all Reviews
+router.get("/", async (req, res) => {
+  try {
+    const reviewData = await Review.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Comment,
+          attributes: ["id", "user_id", "review_id", "comment_body"],
+          include: [
+            {
+              model: User,
+              attributes: ["name"],
+            },
+          ],
+        },
+      ],
+    });
+
+    const reviews = reviewData.map((review) => review.get({ plain: true }));
+    res.render("homepage", {
+      reviews,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one review
+router.get("/review/:id", async (req, res) => {
+  try {
+    const reviewData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Comment,
+          attributes: ["id", "user_id", "review_id", "comment_body"],
+          include: [
+            {
+              model: User,
+              attributes: ["name"],
+            },
+          ],
+        },
+      ],
+    });
+
+    const review = reviewData.get({ plain: true });
+    res.render("one-post", { review, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login route
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+module.exports = router;
+
+// Sign up route
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
+});
+
+module.exports = router;
