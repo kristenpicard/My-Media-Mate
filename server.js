cc20729386debba2e0739527fcb030f6e8362937
Mo Ager <moager@att.net>
2021-05-03 18:23:55 -0400

    Branch issues

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 
 app.use(routes);
 
