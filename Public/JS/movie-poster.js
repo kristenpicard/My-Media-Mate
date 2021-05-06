56d1f8f8887d95a749eb6fbb97958d8bb08b20e8
Keith Tatad <ktatad0621@gmail.com>
2021-05-06 18:10:48 -0400

    added OMDB api key and OMDB api URL string interpolation

--- Public/JS/movie-poster.js
+++ Public/JS/movie-poster.js
@@ -2,4 +2,9 @@ $(".button").on("click", function (event) {
     event.preventDefault();
     let title = $("").val().trim();
     console.log(title);
+
+    const apiKeyOMDB = "73aa1752"
+
+    const omdbURL = "http://www.omdbapi.com?t=" + title + "&apikey=" + apiKeyOMDB;
+    console.log(omdbURL);
 });
