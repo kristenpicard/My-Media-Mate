4b01f1c9e48e0fe9cad817cf0897a596f8c36a16
Kristen Picard <kristenpicard93@gmail.com>
2021-05-07 21:39:09 -0400

    Fix

--- Public/JS/movie-poster.js
+++ Public/JS/movie-poster.js
@@ -1,10 +1,10 @@
-$(".button").on("click", function (event) {
-    event.preventDefault();
-    let title = $("").val().trim();
-    console.log(title);
+// $(".button").on("click", function (event) {
+//     event.preventDefault();
+//     let title = $("").val().trim();
+//     console.log(title);
 
-    const apiKeyOMDB = "73aa1752"
+//     const apiKeyOMDB = "73aa1752"
 
-    const omdbURL = "http://www.omdbapi.com?t=" + title + "&apikey=" + apiKeyOMDB;
-    console.log(omdbURL);
-});
+//     const omdbURL = "http://www.omdbapi.com?t=" + title + "&apikey=" + apiKeyOMDB;
+//     console.log(omdbURL);
+// });
