0b2505c9c49ee04591acc5acd7694e1522baa6a0
Kristen Picard <kristenpicard93@gmail.com>
2021-05-18 19:52:34 -0400

    Adding compression

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 // const helpers = require("handlebars-helpers")();
+var compression = require("compression");
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -32,6 +33,8 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(compression());
+
 // The first public is the web path and the second is the filesystem path of the files being served.
 // This serves all of the files inside the second public directory, and have them accessible through http://localhost:3001/public/FILENAME
 app.use("/Public", express.static(path.join(__dirname, "Public")));
